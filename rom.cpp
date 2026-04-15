8e89cc061f04688da2411827307ccfda93649f64
tylerlumsden <turtletyler03@gmail.com>
2026-04-14 20:53:57 -0400

    Rom writing with hex

---
+++ rom.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <cstdint>
+
+using Byte = uint8_t;
+
+struct Rom {
+    std::vector<Byte> rom_data;
+
+    Rom(std::istream& rom_stream);
+
+    void write_as_hex(std::ostream& out) {
+        for(size_t i = 0; i < rom_data.size(); ++i) {
+            if(i % 8 == 0 && i != 0) {
+                out << "\n";
+            }
+            out << std::hex << static_cast<int>(rom_data[i]) << " ";
+        }
+    }
+};
+
+
+Rom::Rom(std::istream& rom_stream) {
+    char data;
+    while(rom_stream.get(data)) {
+        this->rom_data.push_back(static_cast<Byte>(data));
+    }
+}
+
+int main() {
+    std::ifstream reader("pkmnblue.gb");
+    Rom pkmn_data(reader);
+
+    std::ofstream writer("pkmnblue.dat");
+    pkmn_data.write_as_hex(writer);
+}
